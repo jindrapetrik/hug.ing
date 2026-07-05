b200542eb7f104ff5c3423effcf81f2e93f8cbf5
Jindra Petřík <jindra.petrik@gmail.com>
2026-07-05 09:05:30 +0200

    chore: add db structure

---
+++ sources/hug.table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `hug` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  `recipientKey` char(8) NOT NULL,
+  `senderKey` char(12) NOT NULL,
+  `viewCount` int(11) NOT NULL DEFAULT 0,
+  `acceptCount` int(11) NOT NULL DEFAULT 0,
+  `returnedCount` int(11) NOT NULL DEFAULT 0,
+  `infoDisplayed` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
